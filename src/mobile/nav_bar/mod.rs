a21135ade1648ac76a37bda284d25c074a0f4908
luoxiao <luoxiaozero@163.com>
2023-06-04 17:32:33 +0800

    ✨ feat: NavBar add click

--- src/mobile/nav_bar/mod.rs
+++ src/mobile/nav_bar/mod.rs
@@ -2,6 +2,7 @@ use crate::utils::mount_style::mount_style;
 use leptos::*;
 use leptos_icons::*;
 use stylers::style_sheet_str;
+use web_sys::MouseEvent;
 
 #[component]
 pub fn NavBar(
@@ -9,9 +10,23 @@ pub fn NavBar(
     #[prop(optional, into)] title: MaybeSignal<String>,
     #[prop(optional, into)] left_arrow: MaybeSignal<bool>,
     #[prop(optional, into)] left_text: MaybeSignal<String>,
+    #[prop(optional, into)] click_left: Option<SignalSetter<MouseEvent>>,
     #[prop(optional, into)] right_text: MaybeSignal<String>,
+    #[prop(optional, into)] click_right: Option<SignalSetter<MouseEvent>>,
+
 ) -> impl IntoView {
     let class_name = mount_style("nav-bar", || style_sheet_str!("./src/mobile/nav_bar/nav-bar.css"));
+
+    let onclick_left = move |ev| {
+        if let Some(click_left) = click_left {
+            click_left.set(ev);
+        }
+    };
+    let onclick_right = move |ev| {
+        if let Some(click_right) = click_right {
+            click_right.set(ev);
+        }
+    };
 
     view! { cx, class=class_name,
         <div class="melt-nav-bar">
@@ -20,7 +35,7 @@ pub fn NavBar(
                     let left_text = left_text.get();
                     if left_arrow.get() || !left_text.is_empty() {
                         view! { cx, class=class_name,
-                            <div class="melt-nav-bar__left">
+                            <div class="melt-nav-bar__left" on:click=onclick_left>
                                 {
                                     if left_arrow.get() {
                                         view! { cx,
@@ -46,7 +61,7 @@ pub fn NavBar(
                     let right_text = right_text.get();
                     if !right_text.is_empty() {
                         view! { cx, class=class_name,
-                            <div class="melt-nav-bar__right">
+                            <div class="melt-nav-bar__right" on:click=onclick_right>
                                 { right_text }
                             </div>
                         }.into()
