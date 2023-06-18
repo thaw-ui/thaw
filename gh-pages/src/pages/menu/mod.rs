164e4952655c1bd8448d07d00b69f6702250f8d0
luoxiao <luoxiaozero@163.com>
2023-06-18 23:00:59 +0800

    ✨ feat: update leptos

--- gh-pages/src/pages/menu/mod.rs
+++ gh-pages/src/pages/menu/mod.rs
@@ -5,12 +5,10 @@ use melt_ui::*;
 pub fn MenuPage(cx: Scope) -> impl IntoView {
     let selected = create_rw_signal(cx, String::from("o"));
     view! { cx,
-        <>
-            { move || selected.get() }
-            <Menu selected>
-                <MenuItem key="a" label="and"/>
-                <MenuItem key="o" label="or"/>
-            </Menu>
-        </>
+        { move || selected.get() }
+        <Menu selected>
+            <MenuItem key="a" label="and"/>
+            <MenuItem key="o" label="or"/>
+        </Menu>
     }
 }
