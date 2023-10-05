967d09deaae4e4a06a9a4e2b83ef63e89dd04faf
luoxiao <luoxiaozero@163.com>
2023-10-05 12:23:15 +0800

    refactor(gh-pages): home page

--- gh-pages/src/pages/home.rs
+++ gh-pages/src/pages/home.rs
@@ -1,4 +1,3 @@
-use crate::components::*;
 use leptos::*;
 use leptos_router::{use_navigate, use_query_map};
 use melt_ui::*;
@@ -11,17 +10,24 @@ pub fn Home() -> impl IntoView {
         navigate(path, Default::default());
     }
     view! {
-        <Layout position=LayoutPosition::ABSOLUTE>
-            <SiteHeader />
-            <Layout position=LayoutPosition::ABSOLUTE style="top: 54px; display: flex; align-items: center; justify-content: center; flex-direction: column;">
-                <p>"A Leptos UI Library"</p>
-                <Button on:click=move |_| {
+        <Layout position=LayoutPosition::ABSOLUTE style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
+            <h1 style="font-size: 80px; line-height: 1;margin: 0 0 18px;">
+                "Melt UI"
+            </h1>
+            <p>"An easy to use leptos component library"</p>
+            <Space>
+                <Button on_click=move |_| {
                     let navigate = use_navigate();
                     navigate("/components/menu", Default::default());
                 }>
                     "Read the docs"
                 </Button>
-            </Layout>
+                <Button type_=ButtonType::TEXT on:click=move |_| {
+                    _ = window().open_with_url("http://github.com/luoxiaozero/melt-ui");
+                }>
+                    "Github"
+                </Button>
+            </Space>
         </Layout>
     }
 }
