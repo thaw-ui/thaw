550c87727dbe7c51319a309d2848518608a6d19e
luoxiao <luoxiaozero@163.com>
2023-10-08 17:34:25 +0800

    feat: code component font size

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -1,6 +1,8 @@
 use leptos::*;
+use crate::utils::mount_style::mount_style;
 
 #[component]
 pub fn Code(children: Children) -> impl IntoView {
+    mount_style("code", include_str!("./code.css"));
     view! { <code class="melt-code">{children()}</code> }
 }
