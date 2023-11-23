d900690531d25a96fb531d14cec19654643c6e0e
luoxiao <luoxiaozero@163.com>
2023-11-23 10:24:34 +0800

    fix(hydrate): teleport component

--- src/components/teleport/mod.rs
+++ src/components/teleport/mod.rs
@@ -1,5 +1,6 @@
 use cfg_if::cfg_if;
 use leptos::{html::AnyElement, *};
+
 /// https://github.com/solidjs/solid/blob/main/packages/solid/web/src/index.ts#L56
 #[component]
 pub fn Teleport(
@@ -7,8 +8,11 @@ pub fn Teleport(
     #[prop(optional, into)] element: Option<HtmlElement<AnyElement>>,
     #[prop(optional)] children: Option<Children>,
 ) -> impl IntoView {
-    cfg_if! { if #[cfg(target_arch = "wasm32")] {
+    cfg_if! { if #[cfg(all(target_arch = "wasm32", any(feature = "csr", feature = "hydrate")))] {
         use leptos::wasm_bindgen::JsCast;
+        use leptos::leptos_dom::Mountable;
+        use crate::utils::with_hydration_off;
+
         let mount = mount.unwrap_or_else(|| {
             document()
                 .body()
@@ -16,21 +20,41 @@ pub fn Teleport(
                 .unchecked_into()
         });
 
-        let render_root = if let Some(element) = element {
-            element
+        if let Some(element) = element {
+            let render_root = element;
+            let _  = mount.append_child(&render_root);
+            on_cleanup(move || {
+                let _ = mount.remove_child(&render_root);
+            });
         } else if let Some(children) =  children {
-            html::div().child(children()).into_any()
+            let container = document()
+                .create_element("div")
+                .expect("element creation to work");
+            with_hydration_off(|| {
+                let _ = container.append_child(&children().into_view().get_mountable_node());
+            });
+
+            let render_root = container;
+            let _  = mount.append_child(&render_root);
+            on_cleanup(move || {
+                let _ = mount.remove_child(&render_root);
+            });
         } else {
             return;
         };
-
-        _  = mount.append_child(&render_root);
-        on_cleanup(move || {
-            _ = mount.remove_child(&render_root);
-        });
     } else {
-        _ = mount;
-        _ = element;
-        _ = children;
+        let _ = mount;
+        #[cfg(not(feature = "ssr"))]
+        {
+            let _ = element;
+            let _ = children;
+        }
+        #[cfg(feature = "ssr")]
+        if element.is_none() {
+            if let Some(children) =  children {
+                // Consumed hydration `id`
+                let _  = children();
+            }
+        }
     }}
 }
