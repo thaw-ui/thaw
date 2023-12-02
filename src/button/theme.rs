56af52c559a7de1ec78c5b6722fb03fd4ace51cb
Cristobal Andrada <kandrelczyk@gmail.com>
2023-12-02 19:52:04 +0100

    disabled button - additional theme colors

--- src/button/theme.rs
+++ src/button/theme.rs
@@ -4,6 +4,9 @@ use crate::theme::ThemeMethod;
 pub struct ButtonTheme {
     pub color_text_hover: String,
     pub color_text_active: String,
+    pub color_text_disabled: String,
+    pub color_background_disabled: String,
+    pub color_border_disabled: String
 }
 
 impl ThemeMethod for ButtonTheme {
@@ -11,6 +14,9 @@ impl ThemeMethod for ButtonTheme {
         Self {
             color_text_hover: "#f1f3f5".into(),
             color_text_active: "#eceef0".into(),
+            color_text_disabled: "#737373".into(),
+            color_background_disabled: "#ceced1".into(),
+            color_border_disabled: "#909090".into(),
         }
     }
 
@@ -18,6 +24,9 @@ impl ThemeMethod for ButtonTheme {
         Self {
             color_text_hover: "#ffffff1a".into(),
             color_text_active: "#ffffff26".into(),
+            color_text_disabled: "#ceced1".into(),
+            color_background_disabled: "#737373".into(),
+            color_border_disabled: "#ceced1".into(),
         }
     }
 }
