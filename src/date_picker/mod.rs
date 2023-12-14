ae1c7342354936464c88711158fd83d67b2d75ad
luoxiao <luoxiaozero@163.com>
2023-12-14 15:26:03 +0800

    feat: add date panel

--- src/date_picker/mod.rs
+++ src/date_picker/mod.rs
@@ -1,13 +1,14 @@
+mod panel;
 mod theme;
 
 use crate::{
     chrono::NaiveDate,
     components::{Binder, Follower, FollowerPlacement},
-    use_theme,
     utils::mount_style,
-    AiIcon, Icon, Input, InputSuffix, Theme,
+    AiIcon, Icon, Input, InputSuffix,
 };
 use leptos::*;
+use panel::Panel;
 pub use theme::DatePickerTheme;
 
 #[component]
@@ -69,76 +70,8 @@ pub fn DatePicker(#[prop(optional, into)] value: RwSignal<Option<NaiveDate>>) ->
                 </Input>
             </div>
             <Follower slot show=is_show_panel placement=FollowerPlacement::BottomStart>
-                <Panel date_picker_ref close_panel/>
+                <Panel date_picker_ref close_panel selected_date=panel_selected_date/>
             </Follower>
         </Binder>
-    }
-}
-
-#[component]
-fn Panel(
-    date_picker_ref: NodeRef<html::Div>,
-    close_panel: Callback<Option<NaiveDate>>,
-) -> impl IntoView {
-    let theme = use_theme(Theme::light);
-    let css_vars = create_memo(move |_| {
-        let mut css_vars = String::new();
-        theme.with(|theme| {
-            css_vars.push_str(&format!(
-                "--thaw-item-font-color: {};",
-                theme.common.color_primary
-            ));
-            css_vars.push_str(&format!(
-                "--thaw-background-color: {};",
-                theme.date_picker.panel_background_color
-            ));
-            css_vars.push_str(&format!(
-                "--thaw-item-background-color-hover: {};",
-                theme.date_picker.panel_date_item_background_color_hover
-            ));
-            css_vars.push_str(&format!(
-                "--thaw-item-border-color: {};",
-                theme.date_picker.panel_border_color
-            ));
-        });
-        css_vars
-    });
-
-    let panel_ref = create_node_ref::<html::Div>();
-    #[cfg(any(feature = "csr", feature = "hydrate"))]
-    {
-        use leptos::wasm_bindgen::__rt::IntoJsResult;
-        let handle = window_event_listener(ev::click, move |ev| {
-            let el = ev.target();
-            let mut el: Option<web_sys::Element> =
-                el.into_js_result().map_or(None, |el| Some(el.into()));
-            let body = document().body().unwrap();
-            while let Some(current_el) = el {
-                if current_el == *body {
-                    break;
-                };
-                if panel_ref.get().is_none() {
-                    return;
-                }
-                if current_el == ***panel_ref.get_untracked().unwrap()
-                    || current_el == ***date_picker_ref.get_untracked().unwrap()
-                {
-                    return;
-                }
-                el = current_el.parent_element();
-            }
-            close_panel.call(None);
-        });
-        on_cleanup(move || handle.remove());
-    }
-    #[cfg(not(any(feature = "csr", feature = "hydrate")))]
-    {
-        _ = date_picker_ref;
-        _ = panel_ref;
-    }
-
-    view! {
-        <div class="thaw-date-picker-panel" style=move || css_vars.get() ref=panel_ref>
-        </div>
     }
 }
--- src/date_picker/theme.rs
+++ src/date_picker/theme.rs
@@ -5,6 +5,7 @@ pub struct DatePickerTheme {
     pub panel_background_color: String,
     pub panel_date_item_background_color_hover: String,
     pub panel_border_color: String,
+    pub panel_other_month_font_color: String,
 }
 
 impl ThemeMethod for DatePickerTheme {
@@ -13,6 +14,7 @@ impl ThemeMethod for DatePickerTheme {
             panel_background_color: "#fff".into(),
             panel_date_item_background_color_hover: "#f1f3f5".into(),
             panel_border_color: "#e0e0e6".into(),
+            panel_other_month_font_color: "#c2c2c2".into(),
         }
     }
 
@@ -21,6 +23,7 @@ impl ThemeMethod for DatePickerTheme {
             panel_background_color: "#48484e".into(),
             panel_date_item_background_color_hover: "#ffffff1a".into(),
             panel_border_color: "#ffffff3d".into(),
+            panel_other_month_font_color: "#ffffff61".into(),
         }
     }
 }
