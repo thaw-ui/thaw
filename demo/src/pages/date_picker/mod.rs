4a1a54a4bbcec42ff68a2757f442ed71ad137320
Cristobal Andrada <kandrelczyk@gmail.com>
2023-12-20 23:25:34 +0100

    Add class param to date picker

--- demo/src/pages/date_picker/mod.rs
+++ demo/src/pages/date_picker/mod.rs
@@ -51,6 +51,12 @@ pub fn DatePickerPage() -> impl IntoView {
                             "Set the date picker value"
                         </td>
                     </tr>
+                    <tr>
+                        <td>"class"</td>
+                        <td>"MaybeSignal<String>"</td>
+                        <td>"Default::default()"</td>
+                        <td>"Addtional classes for the date picker element."</td>
+                    </tr>
                 </tbody>
             </Table>
         </div>
