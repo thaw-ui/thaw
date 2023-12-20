f31d540de8bcb482ba25259bb54ed77903629525
luoxiao <luoxiaozero@163.com>
2023-12-20 22:12:32 +0800

    feat: popover add style

--- demo/src/pages/popover/mod.rs
+++ demo/src/pages/popover/mod.rs
@@ -1,5 +1,6 @@
 use crate::components::{Demo, DemoCode};
 use leptos::*;
+use leptos_meta::Style;
 use prisms::highlight_str;
 use thaw::*;
 
@@ -40,129 +41,132 @@ pub fn PopoverPage() -> impl IntoView {
                 </DemoCode>
             </Demo>
             <h3>"Placement"</h3>
+            <Style>
+                ".demo-popover .thaw-button { width: 100% }"
+            </Style>
             <Demo>
-            <Grid x_gap=8 y_gap=8 cols=3>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Top Start"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover placement=PopoverPlacement::Top>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Top"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Top End"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Left Start"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem offset=1>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Right Start"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover placement=PopoverPlacement::Left>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Left"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem offset=1>
-                    <Popover placement=PopoverPlacement::Right>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Right"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Left End"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem offset=1>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Right End"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Bottom Start"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover placement=PopoverPlacement::Bottom>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Bottom"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-                <GridItem>
-                    <Popover>
-                        <PopoverTrigger slot>
-                            <Button>
-                                "Bottom End"
-                            </Button>
-                        </PopoverTrigger>
-                        "Content"
-                    </Popover>
-                </GridItem>
-            </Grid>
+                <Grid x_gap=8 y_gap=8 cols=3 class="demo-popover">
+                    <GridItem>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Top Start"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover placement=PopoverPlacement::Top>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Top"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Top End"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Left Start"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem offset=1>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Right Start"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover placement=PopoverPlacement::Left>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Left"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem offset=1>
+                        <Popover placement=PopoverPlacement::Right>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Right"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Left End"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem offset=1>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Right End"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover placement=PopoverPlacement::BottomStart>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Bottom Start"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover placement=PopoverPlacement::Bottom>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Bottom"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                    <GridItem>
+                        <Popover>
+                            <PopoverTrigger slot>
+                                <Button>
+                                    "Bottom End"
+                                </Button>
+                            </PopoverTrigger>
+                            "Content"
+                        </Popover>
+                    </GridItem>
+                </Grid>
             <DemoCode slot>
 
                 {highlight_str!(
--- src/popover/mod.rs
+++ src/popover/mod.rs
@@ -1,5 +1,13 @@
-use crate::components::{Binder, Follower, FollowerPlacement};
+mod theme;
+
+use crate::{
+    components::{Binder, Follower, FollowerPlacement},
+    use_theme,
+    utils::mount_style,
+    Theme,
+};
 use leptos::*;
+pub use theme::PopoverTheme;
 
 #[slot]
 pub struct PopoverTrigger {
@@ -12,6 +20,18 @@ pub fn Popover(
     #[prop(optional)] placement: PopoverPlacement,
     children: Children,
 ) -> impl IntoView {
+    mount_style("popover", include_str!("./popover.css"));
+    let theme = use_theme(Theme::light);
+    let css_vars = create_memo(move |_| {
+        let mut css_vars = String::new();
+        theme.with(|theme| {
+            css_vars.push_str(&format!(
+                "--thaw-background-color: {};",
+                theme.time_picker.panel_background_color
+            ));
+        });
+        css_vars
+    });
     let target_ref = create_node_ref::<html::Div>();
     let is_show_popover = create_rw_signal(false);
     let on_mouse_enter = move |_| {
@@ -20,14 +40,18 @@ pub fn Popover(
     let on_mouse_leave = move |_| {
         is_show_popover.set(false);
     };
+    let placement_str = placement.as_str();
     view! {
         <Binder target_ref>
-            <div ref=target_ref on:mouseenter=on_mouse_enter on:mouseleave=on_mouse_leave>
+            <div ref=target_ref on:mouseenter=on_mouse_enter on:mouseleave=on_mouse_leave class="thaw-popover-trigger">
                 {(popover_trigger.children)()}
             </div>
             <Follower slot show=is_show_popover placement>
-                <div>
+                <div class=format!("thaw-popover thaw-popover--{placement_str}") style=move || css_vars.get()>
                     {children()}
+                    <div class="thaw-popover__angle-container">
+                        <div class="thaw-popover__angle"></div>
+                    </div>
                 </div>
             </Follower>
         </Binder>
@@ -49,6 +73,18 @@ pub enum PopoverPlacement {
     // RightEnd,
     BottomStart,
     // BottomEnd,
+}
+
+impl PopoverPlacement {
+    fn as_str(&self) -> &'static str {
+        match self {
+            PopoverPlacement::Top => "top",
+            PopoverPlacement::Bottom => "bottom",
+            PopoverPlacement::Left => "left",
+            PopoverPlacement::Right => "right",
+            PopoverPlacement::BottomStart => "bottom-start",
+        }
+    }
 }
 
 impl From<PopoverPlacement> for FollowerPlacement {
