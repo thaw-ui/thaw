c9db5b3dd46aaf00c0b22d0c38b5e718070b7210
luoxiaozero <48741584+luoxiaozero@users.noreply.github.com>
2023-12-25 21:28:52 +0800

    feat: add class param to layout component (#60)

--- src/layout/layout_sider.rs
+++ src/layout/layout_sider.rs
@@ -1,14 +1,15 @@
-use crate::utils::mount_style;
+use crate::utils::{class_list::class_list, mount_style};
 use leptos::*;
 
 #[component]
 pub fn LayoutSider(
+    #[prop(optional, into)] class: MaybeSignal<String>,
     #[prop(optional, into)] style: MaybeSignal<String>,
     children: Children,
 ) -> impl IntoView {
     mount_style("layout-sider", include_str!("./layout-sider.css"));
     view! {
-        <div class="thaw-layout-sider" style=move || style.get()>
+        <div class=class_list!["thaw-layout-sider", move || class.get()] style=move || style.get()>
             {children()}
         </div>
     }
