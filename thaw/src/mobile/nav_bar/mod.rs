0ad581338330669382c015d6d1807c5d09c013e8
Cristobal Andrada <kandrelczyk@gmail.com>
2024-01-14 20:44:00 +0100

    feature: add support for complex NavBar elements

--- thaw/src/mobile/nav_bar/mod.rs
+++ thaw/src/mobile/nav_bar/mod.rs
@@ -10,13 +10,23 @@ use crate::{
 use leptos::*;
 pub use theme::NavBarTheme;
 
+#[derive(Clone)]
+#[slot]
+pub struct NavBarElement {
+    #[prop(optional, into)]
+    class: MaybeSignal<String>,
+    children: ChildrenFn,
+}
+
 #[component]
 pub fn NavBar(
     #[prop(optional, into)] title: MaybeSignal<String>,
     #[prop(optional, into)] left_arrow: MaybeSignal<bool>,
     #[prop(optional, into)] left_text: MaybeSignal<String>,
+    #[prop(optional)] left_element: Option<NavBarElement>,
     #[prop(optional, into)] on_click_left: Option<Callback<ev::MouseEvent>>,
     #[prop(optional, into)] right_text: MaybeSignal<String>,
+    #[prop(optional)] right_element: Option<NavBarElement>,
     #[prop(optional, into)] on_click_right: Option<Callback<ev::MouseEvent>>,
     #[prop(optional, into)] class: MaybeSignal<String>,
 ) -> impl IntoView {
@@ -33,6 +43,8 @@ pub fn NavBar(
     let title: StoredMaybeSignal<_> = title.into();
     let left_text: StoredMaybeSignal<_> = left_text.into();
     let right_text: StoredMaybeSignal<_> = right_text.into();
+    let left_element = store_value(left_element);
+    let right_element = store_value(right_element);
 
     let on_click_left = move |ev| {
         if let Some(click_left) = on_click_left.as_ref() {
@@ -48,7 +60,7 @@ pub fn NavBar(
 
     view! {
         <div class=class_list!["thaw-nav-bar", move || class.get()] style=move || css_vars.get()>
-            <If cond=MaybeSignal::derive(move || left_arrow.get() || !left_text.get().is_empty())>
+            <If cond=MaybeSignal::derive(move || left_arrow.get() || !left_text.get().is_empty() || left_element.get_value().is_some())>
                 <Then slot>
                     <div class="thaw-nav-bar__left" on:click=on_click_left>
                         <If cond=left_arrow>
@@ -57,17 +69,33 @@ pub fn NavBar(
                             </Then>
                         </If>
                         {move || left_text.get()}
+                        <If cond=left_element.get_value().is_some()>
+                            <Then slot>
+                                <div class=class_list!["thaw-nav-bar-element", left_element.get_value().unwrap().class.get()]>
+                                    { (left_element.get_value().unwrap().children)().into_view() }
+                                </div>
+                            </Then>
+                        </If>
+
                     </div>
                 </Then>
             </If>
             <div class="thaw-nav-bar__center">{move || title.get()}</div>
-            <If cond=MaybeSignal::derive(move || !right_text.get().is_empty())>
+            <If cond=MaybeSignal::derive(move || !right_text.get().is_empty() || right_element.get_value().is_some())>
                 <Then slot>
                     <div class="thaw-nav-bar__right" on:click=on_click_right>
                         {move || right_text.get()}
+                        <If cond=right_element.get_value().is_some()>
+                            <Then slot>
+                                <div class=class_list!["thaw-nav-bar-element", right_element.get_value().unwrap().class.get()]>
+                                    { (right_element.get_value().unwrap().children)().into_view() }
+                                </div>
+                            </Then>
+                        </If>
                     </div>
                 </Then>
             </If>
         </div>
     }
 }
+
