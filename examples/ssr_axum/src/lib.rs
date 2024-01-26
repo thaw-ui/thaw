9817acdcfa259d2030d143d63c986a805277ae9d
luoxiao <luoxiaozero@163.com>
2024-01-27 00:06:07 +0800

    feat: update dependencies and update leptos to v0.6.0-rc1

--- examples/ssr_axum/src/lib.rs
+++ examples/ssr_axum/src/lib.rs
@@ -1,17 +1,14 @@
-use cfg_if::cfg_if;
+#[cfg(feature = "ssr")]
 pub mod fileserv;
 
-cfg_if! { if #[cfg(feature = "hydrate")] {
-    use leptos::*;
-    use wasm_bindgen::prelude::wasm_bindgen;
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
     use demo::App;
 
-    #[wasm_bindgen]
-    pub fn hydrate() {
-        // initializes logging using the `log` crate
-        _ = console_log::init_with_level(log::Level::Debug);
-        console_error_panic_hook::set_once();
+    // initializes logging using the `log` crate
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
 
-        leptos::mount_to_body(App);
-    }
-}}
+    leptos::mount_to_body(App);
+}
