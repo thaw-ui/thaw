c924ce51d4dca18ec866132832c3e704430d4b6a
luoxiao <luoxiaozero@163.com>
2024-03-04 14:14:04 +0800

    fix: CSSTransition timeout processing

--- thaw/src/components/css_transition/mod.rs
+++ thaw/src/components/css_transition/mod.rs
@@ -1,7 +1,6 @@
-use leptos::{html::ElementDescriptor, *};
-use std::ops::Deref;
-
 use crate::utils::{add_event_listener, EventListenerHandle};
+use leptos::{html::ElementDescriptor, *};
+use std::{ops::Deref, time::Duration};
 
 /// # CSS Transition
 ///
@@ -25,32 +24,69 @@ where
 
     node_ref.on_load(move |node_el| {
         let any_el = node_el.clone().into_any();
-        let el = any_el.deref();
+        let el = any_el.deref().clone();
         let class_list = el.class_list();
-        let handle = StoredValue::new(None::<(EventListenerHandle, EventListenerHandle)>);
+        let end_handle = StoredValue::new(None::<EventListenerHandle>);
+        let end_count = StoredValue::new(0usize);
 
         let on_end = Callback::new(move |remove: Callback<()>| {
-            let transition_handle =
-                add_event_listener(any_el.clone(), ev::transitionend, move |_| {
-                    remove.call(());
-                    handle.update_value(|h| {
-                        h.take().map(|(t, a)| {
-                            t.remove();
-                            a.remove();
-                        });
+            let Some(CSSTransitionInfo {
+                types,
+                prop_count,
+                timeout,
+            }) = get_transition_info(&el)
+            else {
+                remove.call(());
+                return;
+            };
+            let Some(types) = types else {
+                remove.call(());
+                return;
+            };
+
+            let remove = move || {
+                remove.call(());
+                end_handle.update_value(|h| {
+                    h.take().map(|h| {
+                        h.remove();
                     });
                 });
-            let animation_handle =
-                add_event_listener(any_el.clone(), ev::animationend, move |_| {
-                    remove.call(());
-                    handle.update_value(|h| {
-                        h.take().map(|(t, a)| {
-                            t.remove();
-                            a.remove();
+            };
+
+            end_count.set_value(0);
+
+            set_timeout(
+                move || {
+                    if end_count.with_value(|v| v < &prop_count) {
+                        remove();
+                    }
+                },
+                Duration::from_millis(timeout + 1),
+            );
+
+            let handle = match types {
+                AnimationTypes::Transition => {
+                    add_event_listener(any_el.clone(), ev::transitionend, move |_| {
+                        end_count.update_value(|v| {
+                            *v += 1;
+                            if *v >= prop_count {
+                                remove();
+                            }
                         });
-                    });
-                });
-            handle.set_value(Some((transition_handle, animation_handle)));
+                    })
+                }
+                AnimationTypes::Animation => {
+                    add_event_listener(any_el.clone(), ev::animationend, move |_| {
+                        end_count.update_value(|v| {
+                            *v += 1;
+                            if *v >= prop_count {
+                                remove();
+                            }
+                        });
+                    })
+                }
+            };
+            end_handle.set_value(Some(handle));
         });
 
         let on_enter_fn = {
@@ -130,9 +166,81 @@ where
     children(display.read_only())
 }
 
-
-pub fn next_frame(cb: impl FnOnce() + 'static) {
+fn next_frame(cb: impl FnOnce() + 'static) {
     request_animation_frame(move || {
         request_animation_frame(cb);
     });
+}
+
+#[derive(PartialEq)]
+enum AnimationTypes {
+    Transition,
+    Animation,
+}
+
+#[derive(Default)]
+struct CSSTransitionInfo {
+    types: Option<AnimationTypes>,
+    prop_count: usize,
+    timeout: u64,
+}
+
+fn get_transition_info(el: &web_sys::HtmlElement) -> Option<CSSTransitionInfo> {
+    let styles = window().get_computed_style(el).ok().flatten()?;
+
+    let get_style_properties = |property: &str| {
+        styles
+            .get_property_value(property)
+            .unwrap_or_default()
+            .split(", ")
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>()
+    };
+
+    let transition_delays = get_style_properties("transition-delay");
+    let transition_durations = get_style_properties("transition-duration");
+    let transition_timeout = get_timeout(transition_delays, &transition_durations);
+    let animation_delays = get_style_properties("animation-delay");
+    let animation_durations = get_style_properties("animation-duration");
+    let animation_timeout = get_timeout(animation_delays, &animation_durations);
+
+    let timeout = u64::max(transition_timeout, animation_timeout);
+    let (types, prop_count) = if timeout > 0 {
+        if transition_timeout > animation_timeout {
+            (Some(AnimationTypes::Transition), transition_durations.len())
+        } else {
+            (Some(AnimationTypes::Animation), animation_durations.len())
+        }
+    } else {
+        (None, 0)
+    };
+
+    Some(CSSTransitionInfo {
+        types,
+        prop_count,
+        timeout,
+    })
+}
+
+fn get_timeout(mut delays: Vec<String>, durations: &Vec<String>) -> u64 {
+    while delays.len() < durations.len() {
+        delays.append(&mut delays.clone())
+    }
+
+    fn to_ms(s: &String) -> u64 {
+        if s == "auto" || s.is_empty() {
+            return 0;
+        }
+
+        let s = s.split_at(s.len() - 1).0;
+
+        (s.parse::<f32>().unwrap_or_default() * 1000.0).floor() as u64
+    }
+
+    durations
+        .iter()
+        .enumerate()
+        .map(|(i, d)| to_ms(d) + to_ms(&delays[i]))
+        .max()
+        .unwrap_or_default()
 }
