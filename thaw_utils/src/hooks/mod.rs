00e4b2d11dd84efb453e502965bfce349cf1756b
luoxiao <luoxiaozero@163.com>
2024-03-19 21:49:03 +0800

    refactor: thaw_utils directory

---
+++ thaw_utils/src/hooks/mod.rs
@@ -0,0 +1,5 @@
+mod use_click_position;
+mod use_lock_html_scroll;
+
+pub use use_click_position::use_click_position;
+pub use use_lock_html_scroll::use_lock_html_scroll;
--- thaw_utils/src/lib.rs
+++ thaw_utils/src/lib.rs
@@ -1,27 +1,19 @@
 pub mod class_list;
-mod component_ref;
 mod event_listener;
-mod model;
+mod hooks;
 mod mount_style;
-mod optional_maybe_signal;
 mod optional_prop;
-mod signal;
-mod stored_maybe_signal;
+mod signals;
 mod time;
-mod use_click_position;
-mod use_lock_html_scroll;
 
-pub use component_ref::{create_component_ref, ComponentRef};
 pub use event_listener::*;
-pub use model::Model;
+pub use hooks::{use_click_position, use_lock_html_scroll};
 pub use mount_style::mount_style;
-pub use optional_maybe_signal::OptionalMaybeSignal;
 pub use optional_prop::OptionalProp;
-pub use signal::SignalWatch;
-pub use stored_maybe_signal::*;
+pub use signals::{
+    create_component_ref, ComponentRef, Model, OptionalMaybeSignal, SignalWatch, StoredMaybeSignal,
+};
 pub use time::*;
-pub use use_click_position::*;
-pub use use_lock_html_scroll::*;
 
 pub fn with_hydration_off<T>(f: impl FnOnce() -> T) -> T {
     #[cfg(feature = "hydrate")]
