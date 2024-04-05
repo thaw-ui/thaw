a3768cbdcf6fd4c4f4ea08c789b0f171f05b15ee
luoxiao <luoxiaozero@163.com>
2024-04-05 16:58:27 +0800

    feat: Scrollbar adds scroll horizontally

--- thaw/src/scrollbar/mod.rs
+++ thaw/src/scrollbar/mod.rs
@@ -33,8 +33,11 @@ pub fn Scrollbar(
 
     let container_ref = NodeRef::<html::Div>::new();
     let content_ref = NodeRef::<html::Div>::new();
+    let x_track_ref = NodeRef::<html::Div>::new();
     let y_track_ref = NodeRef::<html::Div>::new();
+    let is_show_x_thumb = RwSignal::new(false);
     let is_show_y_thumb = RwSignal::new(false);
+    let x_track_width = RwSignal::new(0);
     let y_track_height = RwSignal::new(0);
     let container_width = RwSignal::new(0);
     let container_height = RwSignal::new(0);
@@ -43,6 +46,35 @@ pub fn Scrollbar(
     let content_width = RwSignal::new(0);
     let content_height = RwSignal::new(0);
     let thumb_status = StoredValue::new(None::<ThumbStatus>);
+
+    let x_thumb_width = Memo::new(move |_| {
+        let content_width = f64::from(content_width.get());
+        let x_track_width = f64::from(x_track_width.get());
+        let container_width = f64::from(container_width.get());
+        if content_width <= 0.0 {
+            return 0.0;
+        }
+        x_track_width * container_width / content_width
+    });
+    let x_thumb_left = Memo::new(move |_| {
+        let x_track_width = f64::from(x_track_width.get());
+        let x_thumb_width = f64::from(x_thumb_width.get());
+        if x_track_width == x_thumb_width {
+            is_show_x_thumb.set(false);
+            return 0.0;
+        }
+
+        let container_width = f64::from(container_width.get());
+        let container_scroll_left = f64::from(container_scroll_left.get());
+        let content_width = f64::from(content_width.get());
+
+        let diff = content_width - container_width;
+        if diff <= 0.0 {
+            0.0
+        } else {
+            (container_scroll_left / diff) * (x_track_width - x_thumb_width)
+        }
+    });
 
     let y_thumb_height = Memo::new(move |_| {
         let content_height = f64::from(content_height.get());
@@ -54,11 +86,16 @@ pub fn Scrollbar(
         y_track_height * container_height / content_height
     });
     let y_thumb_top = Memo::new(move |_| {
+        let y_track_height = f64::from(y_track_height.get());
+        let y_thumb_height = f64::from(y_thumb_height.get());
+        if y_track_height == y_thumb_height {
+            is_show_y_thumb.set(false);
+            return 0.0;
+        }
+
         let container_height = f64::from(container_height.get());
         let container_scroll_top = f64::from(container_scroll_top.get());
         let content_height = f64::from(content_height.get());
-        let y_track_height = f64::from(y_track_height.get());
-        let y_thumb_height = f64::from(y_thumb_height.get());
 
         let diff = content_height - container_height;
         if diff <= 0.0 {
@@ -83,11 +120,15 @@ pub fn Scrollbar(
             content_width.set(el.offset_width());
             content_height.set(el.offset_height());
         }
+        if let Some(el) = x_track_ref.get() {
+            x_track_width.set(el.offset_width());
+        }
         if let Some(el) = y_track_ref.get() {
             y_track_height.set(el.offset_height());
         }
     };
     let on_mouseenter = move |_| {
+        is_show_x_thumb.set(true);
         is_show_y_thumb.set(true);
         thumb_status.update_value(|thumb_status| {
             if thumb_status.is_some() {
@@ -103,12 +144,66 @@ pub fn Scrollbar(
                 *thumb_status = Some(ThumbStatus::DelayLeave);
             } else {
                 is_show_y_thumb.set(false);
+                is_show_x_thumb.set(false);
             }
         });
     };
 
     let on_scroll = move |_| {
         sync_scroll_state();
+    };
+
+    let x_trumb_mousemove_handle = StoredValue::new(None::<WindowListenerHandle>);
+    let x_trumb_mouseup_handle = StoredValue::new(None::<WindowListenerHandle>);
+    let memo_x_left = StoredValue::new(0);
+    let memo_mouse_x = StoredValue::new(0);
+    let on_x_thumb_mousedown = move |e: ev::MouseEvent| {
+        e.prevent_default();
+        e.stop_propagation();
+        let handle = window_event_listener(ev::mousemove, move |e| {
+            let container_width = container_width.get();
+            let content_width = content_width.get();
+            let x_track_width = x_track_width.get();
+            let x_thumb_width = x_thumb_width.get() as i32;
+
+            let x_diff = e.client_x() - memo_mouse_x.get_value();
+            let to_scroll_left_upper_bound = content_width - container_width;
+            let scroll_left =
+                (x_diff * to_scroll_left_upper_bound) / (x_track_width - x_thumb_width);
+
+            let mut to_scroll_left = memo_x_left.get_value() + scroll_left;
+            to_scroll_left = to_scroll_left.min(to_scroll_left_upper_bound);
+            to_scroll_left = to_scroll_left.max(0);
+
+            if let Some(el) = container_ref.get_untracked() {
+                el.set_scroll_left(to_scroll_left);
+            }
+        });
+        x_trumb_mousemove_handle.set_value(Some(handle));
+        let handle = window_event_listener(ev::mouseup, move |_| {
+            x_trumb_mousemove_handle.update_value(|handle| {
+                if let Some(handle) = handle.take() {
+                    handle.remove();
+                }
+            });
+            x_trumb_mouseup_handle.update_value(|handle| {
+                if let Some(handle) = handle.take() {
+                    handle.remove();
+                }
+            });
+            thumb_status.update_value(|thumb_status| {
+                if let Some(status) = thumb_status.take() {
+                    if status == ThumbStatus::DelayLeave {
+                        is_show_x_thumb.set(false);
+                        is_show_y_thumb.set(false);
+                    }
+                }
+            });
+        });
+        x_trumb_mouseup_handle.set_value(Some(handle));
+        memo_x_left.set_value(container_scroll_left.get());
+        memo_mouse_x.set_value(e.client_x());
+        thumb_status.set_value(Some(ThumbStatus::Enter));
     };
 
     let y_trumb_mousemove_handle = StoredValue::new(None::<WindowListenerHandle>);
@@ -152,6 +247,7 @@ pub fn Scrollbar(
             thumb_status.update_value(|thumb_status| {
                 if let Some(status) = thumb_status.take() {
                     if status == ThumbStatus::DelayLeave {
+                        is_show_x_thumb.set(false);
                         is_show_y_thumb.set(false);
                     }
                 }
@@ -175,7 +271,7 @@ pub fn Scrollbar(
         >
 
             <div class="thaw-scrollbar__container" ref=container_ref on:scroll=on_scroll>
-                <div class="thaw-scrollbar__content" ref=content_ref>{children()}</div>
+                <div class="thaw-scrollbar__content" style="width: fit-content;" ref=content_ref>{children()}</div>
             </div>
             <div class="thaw-scrollbar__track--vertical" ref=y_track_ref>
                 <div
@@ -186,8 +282,14 @@ pub fn Scrollbar(
                     on:mousedown=on_y_thumb_mousedown
                 ></div>
             </div>
-            <div class="thaw-scrollbar__track--horizontal">
-                <div class="thaw-scrollabr__thumb"></div>
+            <div class="thaw-scrollbar__track--horizontal" ref=x_track_ref>
+                <div
+                    class="thaw-scrollabr__thumb"
+                    style:display=move || (!is_show_x_thumb.get()).then_some("none")
+                    style:width=move || format!("{}px", x_thumb_width.get())
+                    style:left=move || format!("{}px", x_thumb_left.get())
+                    on:mousedown=on_x_thumb_mousedown
+                ></div>
             </div>
         </div>
     }
