e939cb18bb4fa2d18d3e8df368078c710447713d
Yota <91780796+wiyota@users.noreply.github.com>
2024-04-05 22:43:27 +0900

    feat: Add min/max props into InputNumber (#153)
    * feat: Add min/max props into InputNumber
    
    Adding range into InputNumber component with min/max props
    
    * refactor: InputNumber component
    
    * docs: Add Min / Max section into InputNumber
    
    * docs: Improvement InputNumber
    
    - Change min/max insertion position.
    - Change the crate that is added to T impl for conciseness.
    - Uniformity of expression: maximum for minimum.

--- thaw/src/input_number/mod.rs
+++ thaw/src/input_number/mod.rs
@@ -1,5 +1,6 @@
 use crate::{Button, ButtonVariant, ComponentRef, Icon, Input, InputRef, InputSuffix};
 use leptos::*;
+use num_traits::Bounded;
 use std::ops::{Add, Sub};
 use std::str::FromStr;
 use thaw_utils::{Model, OptionalProp, StoredMaybeSignal};
@@ -14,9 +15,11 @@ pub fn InputNumber<T>(
     #[prop(optional, into)] class: OptionalProp<MaybeSignal<String>>,
     #[prop(optional)] comp_ref: ComponentRef<InputNumberRef>,
     #[prop(attrs)] attrs: Vec<(&'static str, Attribute)>,
+    #[prop(default = MaybeSignal::Static(T::min_value()), into)] min: MaybeSignal<T>,
+    #[prop(default = MaybeSignal::Static(T::max_value()), into)] max: MaybeSignal<T>,
 ) -> impl IntoView
 where
-    T: Add<Output = T> + Sub<Output = T>,
+    T: Add<Output = T> + Sub<Output = T> + PartialOrd + Bounded,
     T: Default + Clone + FromStr + ToString + 'static,
 {
     let input_value = create_rw_signal(String::default());
@@ -39,6 +42,8 @@ where
         true
     });
     let step: StoredMaybeSignal<_> = step.into();
+    let min: StoredMaybeSignal<_> = min.into();
+    let max: StoredMaybeSignal<_> = max.into();
 
     let add = Callback::<ev::MouseEvent>::new(move |e: ev::MouseEvent| {
         e.prevent_default();
@@ -54,6 +59,24 @@ where
         comp_ref.load(InputNumberRef { input_ref });
     });
 
+    let set_within_range = Callback::<ev::FocusEvent>::new(move |_| {
+        let old_value = value.get_untracked();
+        let min = min.get_untracked();
+        let max = max.get_untracked();
+        if old_value < min {
+            value.set(min);
+        } else if old_value > max {
+            value.set(max);
+        }
+    });
+
+    let minus_disabled = create_memo(move |_| disabled.get() || value.get() <= min.get());
+    let plus_disabled = create_memo(move |_| disabled.get() || value.get() >= max.get());
+    let invalid = create_memo(move |_| {
+        let value = value.get();
+        invalid.get() || value < min.get() || value > max.get()
+    });
+
     view! {
         <Input
             attrs
@@ -64,12 +87,13 @@ where
             disabled
             invalid
             comp_ref=input_ref
+            on_blur=set_within_range
         >
             <InputSuffix slot>
-                <Button disabled variant=ButtonVariant::Link on_click=sub>
+                <Button disabled=minus_disabled variant=ButtonVariant::Link on_click=sub>
                     <Icon icon=icondata_ai::AiMinusOutlined style="font-size: 18px"/>
                 </Button>
-                <Button disabled variant=ButtonVariant::Link on_click=add>
+                <Button disabled=plus_disabled variant=ButtonVariant::Link on_click=add>
                     <Icon icon=icondata_ai::AiPlusOutlined style="font-size: 18px"/>
                 </Button>
             </InputSuffix>
