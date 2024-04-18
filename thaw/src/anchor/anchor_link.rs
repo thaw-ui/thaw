116f42194f9b80a7b21c9f013265fb31304d8418
luoxiao <luoxiaozero@163.com>
2024-04-18 15:01:08 +0800

    feat: Anchor scroll view

--- thaw/src/anchor/anchor_link.rs
+++ thaw/src/anchor/anchor_link.rs
@@ -1,6 +1,7 @@
 use crate::use_anchor;
 use leptos::*;
 use thaw_components::OptionComp;
+use thaw_utils::class_list;
 
 #[component]
 pub fn AnchorLink(
@@ -8,28 +9,45 @@ pub fn AnchorLink(
     #[prop(into)] href: String,
     #[prop(optional)] children: Option<Children>,
 ) -> impl IntoView {
+    let anchor = use_anchor();
+
+    let href_id = StoredValue::new(None::<String>);
+    let is_active = Memo::new(move |_| {
+        href_id.with_value(|href_id| {
+            if href_id.is_none() {
+                false
+            } else {
+                anchor.active_id.with(|active_id| active_id == href_id)
+            }
+        })
+    });
+
     if !href.is_empty() {
-        let anchor = use_anchor();
-        let mut href_id = None::<String>;
-        anchor.0.update(|ids| {
-            if href.starts_with('#') {
-                let id = href[1..].to_string();
-                href_id = Some(id.clone());
-                ids.push(id);
+        if href.starts_with('#') {
+            let id = href[1..].to_string();
+            href_id.set_value(Some(id.clone()));
+            anchor.append_id(id);
+
+            on_cleanup(move || {
+                href_id.with_value(|id| {
+                    if let Some(id) = id {
+                        anchor.remove_id(id);
+                    }
+                });
+            });
+        }
+    }
+    let on_click = move |_| {
+        href_id.with_value(move |href_id| {
+            if let Some(href_id) = href_id {
+                anchor.scroll_into_view(href_id);
             }
         });
+    };
 
-        on_cleanup(move || {
-            anchor.0.update(|ids| {
-                if let Some(index) = ids.iter().position(|id| Some(id) == href_id.as_ref()) {
-                    ids.remove(index);
-                }
-            });
-        });
-    }
     view! {
-        <div class="thaw-anchor-link">
-            <a href=href class="thaw-anchor-link__title">
+        <div class=class_list!["thaw-anchor-link", ("thaw-anchor-link--active", move || is_active.get())]>
+            <a href=href class="thaw-anchor-link__title" on:click=on_click>
                 {move || title.get()}
             </a>
             <OptionComp value=children let:children>
--- thaw/src/anchor/mod.rs
+++ thaw/src/anchor/mod.rs
@@ -4,10 +4,10 @@ mod theme;
 pub use anchor_link::AnchorLink;
 pub use theme::AnchorTheme;
 
-use leptos::*;
-use thaw_utils::{add_event_listener_with_bool, mount_style, throttle};
-
 use crate::{use_theme, Theme};
+use leptos::*;
+use std::cmp::Ordering;
+use thaw_utils::{add_event_listener_with_bool, mount_style, throttle};
 
 #[component]
 pub fn Anchor(
@@ -27,21 +27,47 @@ pub fn Anchor(
         css_vars
     });
     let element_ids = RwSignal::new(Vec::<String>::new());
+    let active_id = RwSignal::new(None::<String>);
     let on_scroll = move || {
         element_ids.with(|ids| {
             let offset_target_top = offset_target
                 .as_ref()
                 .map_or(0.0, |el| el.get_bounding_client_rect().top());
 
-            let links: Vec<(f64, f64, f64)> = vec![];
+            let mut links: Vec<LinkInfo> = vec![];
             for id in ids.iter() {
                 if let Some(link_el) = document().get_element_by_id(id) {
                     let link_rect = link_el.get_bounding_client_rect();
-                    let top = link_rect.top() - offset_target_top;
-                    let height = link_rect.height();
+                    links.push(LinkInfo {
+                        top: link_rect.top() - offset_target_top,
+                        id: id.clone(),
+                    });
                 }
             }
-            links
+            links.sort_by(|a, b| {
+                if a.top > b.top {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            });
+
+            let mut temp_link = None::<LinkInfo>;
+            for link in links.into_iter() {
+                if link.top >= 0.0 {
+                    if link.top <= 12.0 {
+                        temp_link = Some(link);
+                        break;
+                    } else if temp_link.is_some() {
+                        break;
+                    } else {
+                        temp_link = None;
+                    }
+                } else {
+                    temp_link = Some(link);
+                }
+            }
+            active_id.set(temp_link.map(|link| link.id));
         });
     };
     let cb = throttle(
@@ -67,7 +93,7 @@ pub fn Anchor(
             </div>
             <div class="thaw-anchor-background">
             </div>
-            <Provider value=AnchorInjection(element_ids)>
+            <Provider value=AnchorInjection::new(element_ids, active_id)>
                 {children()}
             </Provider>
         </div>
@@ -75,8 +101,48 @@ pub fn Anchor(
 }
 
 #[derive(Clone)]
-pub(crate) struct AnchorInjection(pub RwSignal<Vec<String>>);
+pub(crate) struct AnchorInjection {
+    element_ids: RwSignal<Vec<String>>,
+    pub active_id: RwSignal<Option<String>>,
+}
+
+impl Copy for AnchorInjection {}
+
+impl AnchorInjection {
+    fn new(element_ids: RwSignal<Vec<String>>, active_id: RwSignal<Option<String>>) -> Self {
+        Self {
+            element_ids,
+            active_id,
+        }
+    }
+
+    pub fn scroll_into_view(&self, id: &String) {
+        let Some(link_el) = document().get_element_by_id(id) else {
+            return;
+        };
+        link_el.scroll_into_view();
+    }
+
+    pub fn append_id(&self, id: String) {
+        self.element_ids.update(|ids| {
+            ids.push(id);
+        });
+    }
+
+    pub fn remove_id(&self, id: &String) {
+        self.element_ids.update(|ids| {
+            if let Some(index) = ids.iter().position(|item_id| item_id == id) {
+                ids.remove(index);
+            }
+        });
+    }
+}
 
 pub(crate) fn use_anchor() -> AnchorInjection {
     expect_context()
+}
+
+struct LinkInfo {
+    top: f64,
+    id: String,
 }
