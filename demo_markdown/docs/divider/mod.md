c6f83aa01dcae6d331c66a27be262ee8f63f86a1
luoxiao <luoxiaozero@163.com>
2024-05-10 23:26:09 +0800

    refactor: divider

--- demo_markdown/docs/divider/mod.md
+++ demo_markdown/docs/divider/mod.md
@@ -2,9 +2,29 @@
 
 ```rust demo
 view! {
-    "top"
-    <Divider/>
-    "bottom"
+    <Space vertical=true>
+        <div style="padding: 30px 0; background-color: var(--colorNeutralBackground1);">
+            <Divider />
+        </div>
+        <div style="padding: 30px 0; background-color: var(--colorNeutralBackground1);">
+            <Divider>"Text"</Divider>
+        </div>
+    </Space>
+}
+```
+
+### Vertical
+
+```rust demo
+view! {
+    <Space vertical=true gap=SpaceGap::Large>
+        <div style="height: 100px; background-color: var(--colorNeutralBackground1);">
+            <Divider vertical=true/>
+        </div>
+        <div style="height: 100px; background-color: var(--colorNeutralBackground1);">
+            <Divider vertical=true>"Text"</Divider>
+        </div>
+    </Space>
 }
 ```
 
--- thaw/src/divider/mod.rs
+++ thaw/src/divider/mod.rs
@@ -1,13 +1,26 @@
 use leptos::*;
+use thaw_components::OptionComp;
 use thaw_utils::{class_list, mount_style, OptionalProp};
 
 #[component]
-pub fn Divider(#[prop(optional, into)] class: OptionalProp<MaybeSignal<String>>) -> impl IntoView {
+pub fn Divider(
+    #[prop(optional, into)] class: OptionalProp<MaybeSignal<String>>,
+    #[prop(optional, into)] vertical: MaybeSignal<bool>,
+    #[prop(optional)] children: Option<Children>,
+) -> impl IntoView {
     mount_style("divider", include_str!("./divider.css"));
 
     view! {
-        <div class=class_list!["thaw-divider", class.map(| c | move || c.get())]>
-            <div class="thaw-divider__line"></div>
+        <div
+            class=class_list!["thaw-divider", ("thaw-divider--vertical", move || vertical.get()), class.map(| c | move || c.get())]
+            aria-orientation=move || if vertical.get() { "vertical" } else { "horizontal" }
+            role="separator"
+        >
+            <OptionComp value=children let:children>
+                <div class="thaw-divider__wrapper">
+                    {children()}
+                </div>
+            </OptionComp>
         </div>
     }
 }
