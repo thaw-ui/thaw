68659411b93b28c27acbc884fe7815aaeb8ab24f
luoxiao <luoxiaozero@163.com>
2024-07-29 17:03:17 +0800

    feat: add console_error_panic_hook

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -6,5 +6,8 @@ use app::App;
 use leptos::prelude::*;
 
 fn main() {
+    let _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    
     mount_to_body(App)
 }
