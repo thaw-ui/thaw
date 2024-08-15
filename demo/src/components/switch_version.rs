1e175651147c0c42cb810ee9a9f0f2f8a3c35519
luoxiao <luoxiaozero@163.com>
2024-08-15 22:34:43 +0800

    feat: update leptos to v0.7.0-beta2

--- demo/src/components/switch_version.rs
+++ demo/src/components/switch_version.rs
@@ -13,25 +13,23 @@ pub fn SwitchVersion() -> impl IntoView {
         ("0.2.5", "https://thaw-8og1kv8zs-thaw.vercel.app"),
     ];
 
-    cfg_if::cfg_if! {
-        if #[cfg(any(feature = "csr", feature = "hydrate"))] {
-            let location = window().location();
-            let origin = location.origin().ok();
-            let version = RwSignal::new(origin);
-            let _ = version.watch(move |origin| {
-                if let Some(origin) = origin {
-                    let pathname = location.pathname().unwrap_or_default();
-                    let href = format!("{}{}", origin, pathname);
-                    let _ = location.set_href(&href);
-                }
-            });
-        } else {
-            // let version = RwSignal::new(None::<String>);
-        }
+    let version = RwSignal::new(None::<String>);
+    #[cfg(any(feature = "csr", feature = "hydrate"))]
+    {
+        let location = window().location();
+        let origin = location.origin().ok();
+        version.set(origin);
+        let _ = version.watch(move |origin| {
+            if let Some(origin) = origin {
+                let pathname = location.pathname().unwrap_or_default();
+                let href = format!("{}{}", origin, pathname);
+                let _ = location.set_href(&href);
+            }
+        });
     }
 
     view! {
-        <Combobox>
+        <Combobox selected_options=version placeholder="Switch version">
             {
                 options.into_iter().map(|option| view! {
                     <ComboboxOption value=option.1 text=option.0 />
