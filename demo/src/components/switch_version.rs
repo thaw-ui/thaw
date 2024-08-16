e0a671b43783c22285656e9eb9ad60a10e511ddb
luoxiao <luoxiaozero@163.com>
2024-08-16 15:05:50 +0800

    fix: SwitchVersion label

--- demo/src/components/switch_version.rs
+++ demo/src/components/switch_version.rs
@@ -14,10 +14,16 @@ pub fn SwitchVersion() -> impl IntoView {
     ];
 
     let version = RwSignal::new(None::<String>);
+    let label = RwSignal::new(String::new());
     #[cfg(any(feature = "csr", feature = "hydrate"))]
     {
         let location = window().location();
         let origin = location.origin().ok();
+        if let Some(origin) = origin.as_ref() {
+            if let Some(item) = options.iter().find(|item| item.1 == origin) {
+                label.set(item.0.to_string());
+            }
+        }
         version.set(origin);
         let _ = version.watch(move |origin| {
             if let Some(origin) = origin {
@@ -29,7 +35,7 @@ pub fn SwitchVersion() -> impl IntoView {
     }
 
     view! {
-        <Combobox selected_options=version placeholder="Switch version">
+        <Combobox value=label selected_options=version placeholder="Switch version">
             {
                 options.into_iter().map(|option| view! {
                     <ComboboxOption value=option.1 text=option.0 />
