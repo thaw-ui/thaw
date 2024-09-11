2e2a261fe818b67af16ad557ff38477d0f0a0c6e
luoxiao <luoxiaozero@163.com>
2024-09-11 23:32:26 +0800

    feat: Spinner adds children prop

--- thaw/src/spinner/mod.rs
+++ thaw/src/spinner/mod.rs
@@ -38,14 +38,16 @@ pub fn Spinner(
     /// The size of the spinner.
     #[prop(optional, into)]
     size: MaybeSignal<SpinnerSize>,
+    #[prop(optional)] children: Option<Children>,
 ) -> impl IntoView {
     mount_style("spinner", include_str!("./spinner.css"));
     let id = StoredValue::new(uuid::Uuid::new_v4().to_string());
 
     let spinner_label = label.clone();
+    let children_flag = children.is_some();
     let labelledby = move || {
         spinner_label.with(|label| {
-            if label.is_some() {
+            if label.is_some() || children_flag {
                 Some(id.get_value())
             } else {
                 None
@@ -66,17 +68,29 @@ pub fn Spinner(
             <span class="thaw-spinner__spinner">
                 <span class="thaw-spinner__spinner-tail"></span>
             </span>
-            {move || {
-                if let Some(label) = label.get() {
-                    view! {
-                        <label class="thaw-spinner__label" id=id.get_value()>
-                            {label}
-                        </label>
-                    }
-                        .into()
-                } else {
-                    None
+            {if let Some(children) = children {
+                view! {
+                    <label class="thaw-spinner__label" id=id.get_value()>
+                        {children()}
+                    </label>
                 }
+                    .into_any()
+            } else {
+                {
+                    move || {
+                        if let Some(label) = label.get() {
+                            view! {
+                                <label class="thaw-spinner__label" id=id.get_value()>
+                                    {label}
+                                </label>
+                            }
+                                .into()
+                        } else {
+                            None
+                        }
+                    }
+                }
+                    .into_any()
             }}
         </div>
     }
