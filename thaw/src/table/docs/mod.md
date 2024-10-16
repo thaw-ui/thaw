3a96d9fe52294fa7379e40dc349fff23dc2cbc2b
luoxiaozero <48741584+luoxiaozero@users.noreply.github.com>
2024-10-16 21:16:39 +0800

    Feat/table resizable (#282)
    * feat: TableCellLayout adds truncate prop
    
    * feat: TableCellLayout resize handle
    
    * feat: TableCellLayout mousedown
    
    * feat: TableHeaderCell adds min_width prop

--- thaw/src/table/docs/mod.md
+++ thaw/src/table/docs/mod.md
@@ -38,9 +38,67 @@ view! {
 }
 ```
 
-### Table & TableHeader & TableHeaderCell & TableBody & TableRow & TableCell & TableCellLayout Props
+### Resizable Columns
+
+```rust demo
+view! {
+    <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHeaderCell resizable=true min_width=100.0 max_width=300.0>"Tag"</TableHeaderCell>
+                <TableHeaderCell resizable=true>"Count"</TableHeaderCell>
+                <TableHeaderCell>"Date"</TableHeaderCell>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow>
+                <TableCell>
+                    <TableCellLayout truncate=true>
+                        "Renders content with overflow: hidden and text-overflow: ellipsis"
+                    </TableCellLayout>
+                </TableCell>
+                <TableCell>
+                    <TableCellLayout truncate=true>
+                        "Renders content with overflow: hidden and text-overflow: ellipsis"
+                    </TableCellLayout>
+                </TableCell>
+                <TableCell>
+                    <TableCellLayout>
+                        "2023-10-08"
+                    </TableCellLayout>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell>"span"</TableCell>
+                <TableCell>"2"</TableCell>
+                <TableCell>"2023-10-08"</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+}
+```
+
+### Table & TableHeader & TableBody & TableRow & TableCell Props
 
 | Name     | Type                | Default              | Description |
 | -------- | ------------------- | -------------------- | ----------- |
 | class    | `MaybeProp<String>` | `Default::default()` |             |
 | children | `Children`          |                      |             |
+
+### TableHeaderCell Props
+
+| Name      | Type                | Default              | Description                                                 |
+| --------- | ------------------- | -------------------- | ----------------------------------------------------------- |
+| class     | `MaybeProp<String>` | `Default::default()` |                                                             |
+| resizable | `bool`              | `false`              | Whethe the column width can be dragged.                     |
+| min_width | `Option<f64>`       | `None`               | Min width of the column. Only works when resizable is true. |
+| max_width | `Option<f64>`       | `None`               | Max width of the column. Only works when resizable is true. |
+| children  | `Children`          |                      |                                                             |
+
+### TableCellLayout Props
+
+| Name | Type | Default | Description |
+| --- | --- | --- | --- |
+| class | `MaybeProp<String>` | `Default::default()` |  |
+| truncate | `MaybeSignal<bool>` | `false` | Renders content with overflow: hidden and text-overflow: ellipsis |
+| children | `Children` |  |  |
--- thaw/src/table/mod.rs
+++ thaw/src/table/mod.rs
@@ -1,4 +1,4 @@
-use leptos::{either::Either, prelude::*};
+use leptos::{either::Either, ev, html, prelude::*};
 use thaw_utils::{class_list, mount_style};
 
 #[component]
@@ -22,10 +22,80 @@ pub fn TableHeader(
 #[component]
 pub fn TableHeaderCell(
     #[prop(optional, into)] class: MaybeProp<String>,
+    #[prop(optional, into)] resizable: bool,
+    #[prop(optional, into)] min_width: Option<f64>,
+    #[prop(optional, into)] max_width: Option<f64>,
     #[prop(optional)] children: Option<Children>,
 ) -> impl IntoView {
+    let th_ref = NodeRef::<html::Th>::new();
+    let mouse_down_x = RwSignal::new(0);
+    let mouse_down_th_width = RwSignal::new(0.0);
+    let mouse = StoredValue::new(Vec::<WindowListenerHandle>::new());
+
+    let on_mouse_down = move |e: ev::MouseEvent| {
+        mouse_down_x.set(e.x());
+        let Some(th_el) = th_ref.get_untracked() else {
+            return;
+        };
+        let Ok(Some(css)) = window().get_computed_style(&th_el) else{
+           return;
+        };
+        if let Ok(width) = css.get_property_value("width") {
+            let width = web_sys::js_sys::Number::parse_float(&width);
+            mouse_down_th_width.set(width);
+        }
+
+        let on_mouse_move = window_event_listener(ev::mousemove, move |e: ev::MouseEvent| {
+            let mouse_x = e.x();
+            let mut new_width = mouse_down_th_width.get_untracked()
+                + f64::from(mouse_x - mouse_down_x.get_untracked());
+
+            if let Some(max_width) = max_width {
+                if new_width > max_width {
+                    new_width = max_width;
+                }
+            }
+            if let Some(min_width) = min_width {
+                if new_width < min_width {
+                    new_width = min_width;
+                }
+            }
+            if new_width < 0.0 {
+                new_width = 0.0;
+            }
+
+            if let Some(th_el) = th_ref.get_untracked() {
+                let mut style = format!("width: {new_width:.2}px");
+                if let Some(max_width) = max_width {
+                    style.push_str(&format!(";max-width: {max_width:.2}px"));
+                }
+                if let Some(min_width) = min_width {
+                    style.push_str(&format!(";min-width: {min_width:.2}px"));
+                }
+                let _ = th_el.set_attribute("style", &style);
+            }
+        });
+        let on_mouse_up = window_event_listener(ev::mouseup, move |_| {
+            mouse.update_value(|value| {
+                for handle in value.drain(..) {
+                    handle.remove();
+                }
+            });
+        });
+        mouse.update_value(|value| {
+            value.push(on_mouse_move);
+            value.push(on_mouse_up);
+        });
+    };
+    on_cleanup(move || {
+        mouse.update_value(|value| {
+            for handle in value.drain(..) {
+                handle.remove();
+            }
+        });
+    });
     view! {
-        <th class=class_list!["thaw-table-header-cell", class]>
+        <th class=class_list!["thaw-table-header-cell", class] node_ref=th_ref>
             <button class="thaw-table-header-cell__button" role="presentation">
                 {if let Some(children) = children {
                     Either::Left(children())
@@ -33,6 +103,16 @@ pub fn TableHeaderCell(
                     Either::Right(())
                 }}
             </button>
+            {if resizable {
+                Either::Left(view! {
+                    <span class="thaw-table-header-cell__aside" on:mousedown=on_mouse_down>
+                        <div class="thaw-table-resize-handle" role="separator" aria-hidden="true">
+                        </div>
+                    </span>
+                })
+            } else {
+                Either::Right(())
+            }}
         </th>
     }
 }
@@ -74,7 +154,18 @@ pub fn TableCell(
 #[component]
 pub fn TableCellLayout(
     #[prop(optional, into)] class: MaybeProp<String>,
+    #[prop(optional, into)] truncate: MaybeSignal<bool>,
     children: Children,
 ) -> impl IntoView {
-    view! { <div class=class_list!["thaw-table-cell-layout", class]>{children()}</div> }
+    view! {
+        <div class=class_list![
+            "thaw-table-cell-layout",
+            ("thaw-table-cell-layout--truncate", move || truncate.get()),
+            class
+        ]>
+            <div class="thaw-table-cell-layout__content">
+                <span class="thaw-table-cell-layout__main">{children()}</span>
+            </div>
+        </div>
+    }
 }
