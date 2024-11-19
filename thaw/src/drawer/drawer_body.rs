8991bc522f91d13c0f752cc736c6e6d143e0f847
Ari Seyhun <ariseyhun@live.com.au>
2024-11-19 17:45:50 +0800

    fix: add missing Scrollbar import and remove signal from scrollable

--- thaw/src/drawer/drawer_body.rs
+++ thaw/src/drawer/drawer_body.rs
@@ -1,20 +1,24 @@
 use leptos::prelude::*;
 use thaw_utils::class_list;
 
+use crate::Scrollbar;
+
 #[component]
 pub fn DrawerBody(
     #[prop(optional, into)] class: MaybeProp<String>,
-    #[prop(into, default = true)] scrollable: Signal<bool>,
+    #[prop(default = true)] scrollable: bool,
     children: Children,
 ) -> impl IntoView {
-    move || match scrollable.get() {
+    match scrollable {
         true => view! {
             <Scrollbar>
                 <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
             </Scrollbar>
-        },
+        }
+        .into_any(),
         false => view! {
             <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
-        },
+        }
+        .into_any(),
     }
 }
