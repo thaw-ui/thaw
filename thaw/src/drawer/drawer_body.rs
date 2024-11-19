448f63bcd4f336f640bd32eaac18da40c8b0ab50
Ari Seyhun <ariseyhun@live.com.au>
2024-11-19 17:36:58 +0800

    feat: add `scrollable` prop to `DrawerBody`

--- thaw/src/drawer/drawer_body.rs
+++ thaw/src/drawer/drawer_body.rs
@@ -1,15 +1,20 @@
-use crate::Scrollbar;
 use leptos::prelude::*;
 use thaw_utils::class_list;
 
 #[component]
 pub fn DrawerBody(
     #[prop(optional, into)] class: MaybeProp<String>,
+    #[prop(into, default = true)] scrollable: Signal<bool>,
     children: Children,
 ) -> impl IntoView {
-    view! {
-        <Scrollbar>
+    move || match scrollable.get() {
+        true => view! {
+            <Scrollbar>
+                <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
+            </Scrollbar>
+        },
+        false => view! {
             <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
-        </Scrollbar>
+        },
     }
 }
