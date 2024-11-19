b80b0115b1ad647298b17c9635de06c4600abd14
luoxiaozero <48741584+luoxiaozero@users.noreply.github.com>
2024-11-20 00:57:02 +0800

    feat: The scrollable prop of DrawerBody is changed to native_scrollbar (#318)

--- thaw/src/drawer/drawer_body.rs
+++ thaw/src/drawer/drawer_body.rs
@@ -1,24 +1,24 @@
-use leptos::prelude::*;
-use thaw_utils::class_list;
-
 use crate::Scrollbar;
+use leptos::{either::Either, prelude::*};
+use thaw_utils::class_list;
 
 #[component]
 pub fn DrawerBody(
     #[prop(optional, into)] class: MaybeProp<String>,
-    #[prop(default = true)] scrollable: bool,
+    /// Whether to use native scrollbar on itself.
+    #[prop(optional)]
+    native_scrollbar: bool,
     children: Children,
 ) -> impl IntoView {
-    match scrollable {
-        true => view! {
-            <Scrollbar>
-                <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
-            </Scrollbar>
-        }
-        .into_any(),
-        false => view! {
-            <div class=class_list!["thaw-drawer-body", class]>{children()}</div>
-        }
-        .into_any(),
+    view! {
+        <div class=class_list![
+            "thaw-drawer-body", class
+        ]>
+            {if native_scrollbar {
+                Either::Left(children())
+            } else {
+                Either::Right(view! { <Scrollbar>{children()}</Scrollbar> })
+            }}
+        </div>
     }
 }
