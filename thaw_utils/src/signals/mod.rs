6c6959ce20df83efbdce41241b5474612d269f45
luoxiao <luoxiaozero@163.com>
2024-11-22 09:04:59 +0800

    refactor: move model file

--- thaw_utils/src/signals/mod.rs
+++ thaw_utils/src/signals/mod.rs
@@ -3,5 +3,5 @@ mod model;
 mod signal_watch;
 
 pub use component_ref::ComponentRef;
-pub use model::{Model, OptionModel, OptionModelWithValue, VecModel, VecModelWithValue};
+pub use model::*;
 pub use signal_watch::SignalWatch;
