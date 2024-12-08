2a908c19d85bf6001deb1ec32c6a44e93b5e02b5
luoxiaozero <48741584+luoxiaozero@users.noreply.github.com>
2024-12-08 16:45:53 +0800

    feat: adds SSRMountStyleProvider component (#335)

--- examples/ssr_axum/src/app.rs
+++ examples/ssr_axum/src/app.rs
@@ -1,21 +1,24 @@
 pub use demo::App;
 use leptos::prelude::*;
 use leptos_meta::MetaTags;
+use thaw::ssr::SSRMountStyleProvider;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <AutoReload options=options.clone() />
-                <HydrationScripts options/>
-                <MetaTags/>
-            </head>
-            <body>
-                <App/>
-            </body>
-        </html>
+        <SSRMountStyleProvider>
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <AutoReload options=options.clone() />
+                    <HydrationScripts options/>
+                    <MetaTags/>
+                </head>
+                <body>
+                    <App/>
+                </body>
+            </html>
+        </SSRMountStyleProvider>
     }
 }
